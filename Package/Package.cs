64421a0da69b4eb330ce6ee1cef2280510ea7478
The Unstoppable <44943820+TheUnstoppable01@users.noreply.github.com>
2019-07-17 01:18:29 +0300

    Tiny update.

--- Package/Package.cs
+++ Package/Package.cs
@@ -1,7 +1,7 @@
 ﻿/*  
     Copyright (c) 2019 MasterCan
 
-    You can redistribute or modify this code under GNU General Public License.
+    You can redistribute or modify this code under GNU General Public License v3.0.
     The permission given to run this code in a closed source project modified.
     But, you have to release the source code using this library must be released.
     Or, you have to add original owner's name into your project.
