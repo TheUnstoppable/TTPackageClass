edb668a2148dca03c9aad7f3fb4c708d8676614a
The Unstoppable <canozkan145@gmail.com>
2019-07-17 12:22:26 +0300

    Changed the ordering style of headers.

--- Package/Package.cs
+++ Package/Package.cs
@@ -17,7 +17,7 @@ using System.Threading.Tasks;
 
 namespace Package
 {
-    //Last compiled at 2019/07/17 Wednesday 01:15:13 GMT+03.
+    //Last compiled at 2019/07/17 Wednesday 12:21:02 GMT+03.
     public abstract class TTPackageClass
     {
         public string PackageID = "00000000";
@@ -32,13 +32,10 @@ namespace Package
     {
         private static bool ConditionsOK(Stream str)
         {
-            if (str.Length < 4)
+            if(str.Position != str.Length) //I'm afraid this might cause a freeze...
+                return true;
+            else
                 return false;
-
-            if (Expressions.GetText(str, 0, 4).ToUpper() != "ELIF")
-                return false;
-
-            return true;
         }
 
         public static TTPackageClass FromFile(string Location)
@@ -50,72 +47,71 @@ namespace Package
             using (Stream Bytes = new MemoryStream(TPI))
             {
                 TTPackageClass Pack = new TTPackage();
-                if (Expressions.GetText(Bytes, 0, 4).ToUpper() == "DAEH")
-                {
-                    //Processing the HEAD.
-                    int Next = Expressions.GetByte(Bytes, 0);
-                    byte[] Data = Expressions.GetBytes(Bytes, 0, Next);
-                    Data = Data.SubArray(3, Data.Length - 3);
-                    Pack.PackageID = Expressions.GetCRCID(Data.SubArray(0, 4));
-                    Pack.FileCount = Data[Data.Length - 1];
-
-                    //We're done with HEAD, now let's read next 3 null characters.
-                    Expressions.GetBytes(Bytes, 0, 3);
-
-                    //Now, we should have DATA. Ending this if else.
-                }
-                else
-                {
-                    throw new NotSupportedException("TPI file is invalid or corrupt. DEBUG: Couldn't find HEAD.");
-                }
-
-                if (Expressions.GetText(Bytes, 0, 4).ToUpper() == "ATAD")
-                {
-                    //Processing the DATA.
-                    int Next = Expressions.GetByte(Bytes, 0);
-                    Expressions.GetBytes(Bytes, 0, 3); //Wasting the NULs.
-
-                    //Fun part ;)
-                    int NameLen = Expressions.GetByte(Bytes, 0) + 1;
-                    Pack.PackageName = Expressions.GetText(Bytes, 0, NameLen).Substring(1, NameLen - 1);
-                    int VerLen = Expressions.GetByte(Bytes, 0) + 1;
-                    Pack.PackageVer = Expressions.GetText(Bytes, 0, VerLen).Substring(1, VerLen - 1);
-                    int OwnerLen = Expressions.GetByte(Bytes, 0) + 1;
-                    Pack.PackageOwner = Expressions.GetText(Bytes, 0, OwnerLen).Substring(1, OwnerLen - 1);
-
-                    //Unfortunately, I don't know what that next byte are here for. So, I'll discard it.
-                    Expressions.GetBytes(Bytes, 0, 1);
-
-                    //We're done with DATA, now let's read next 3 null characters.
-                    Expressions.GetBytes(Bytes, 0, 3);
-
-                    //Now, we should have FILE. Ending this if else and continuing with a while loop.
-                }
-                else
-                {
-                    throw new NotSupportedException("TPI file is invalid or corrupt. DEBUG: Couldn't find DATA.");
-                }
-
                 while (ConditionsOK(Bytes))
                 {
-                    TTFileClass File = new TTFileClass();
-                    File.FullNameLength = Expressions.GetByte(Bytes, 0) - 1;
-                    Expressions.GetBytes(Bytes, 0, 3); // 3 NULs...
-                    File.CRC = Expressions.GetCRCID(Expressions.GetBytes(Bytes, 0, 4));
+                    string Header = Expressions.GetText(Bytes, 0, 4).ToUpper();
+                    if (Header == "DAEH")
+                    {
+                        //Processing the HEAD.
+                        int Next = Expressions.GetByte(Bytes, 0);
+                        byte[] Data = Expressions.GetBytes(Bytes, 0, Next);
+                        Data = Data.SubArray(3, Data.Length - 3);
+                        Pack.PackageID = Expressions.GetCRCID(Data.SubArray(0, 4));
+                        Pack.FileCount = Data[Data.Length - 1];
 
-                    //Unfortunately, I don't know what these next 2 bytes are here for. So, I'll discarding them.
-                    //Note: A friend says it's file size. But multiplation, addition and XOR did not worked to get original file sizes.
-                    Expressions.GetBytes(Bytes, 0, 2);
+                        //We're done with HEAD, now let's read next 3 null characters.
+                        Expressions.GetBytes(Bytes, 0, 3);
 
-                    //Skipping 2 NULs...
-                    Expressions.GetBytes(Bytes, 0, 2);
+                        //Now, we should have DATA. Ending this if else.
+                    }
+                    else if (Header == "ATAD")
+                    {
+                        //Processing the DATA.
+                        int Next = Expressions.GetByte(Bytes, 0);
+                        int Current = 0;
+                        Expressions.GetBytes(Bytes, 0, 3); //Wasting the NULs.
 
-                    File.OriginalNameLength = Expressions.GetByte(Bytes, 0);
-                    File.FileName = Expressions.GetText(Bytes, 0, File.OriginalNameLength + 1).Substring(1, File.OriginalNameLength);
+                        //Fun part ;)
+                        int NameLen = Expressions.GetByte(Bytes, 0) + 1;
+                        Pack.PackageName = Expressions.GetText(Bytes, 0, NameLen).Substring(1, NameLen - 1);
+                        int VerLen = Expressions.GetByte(Bytes, 0) + 1;
+                        Pack.PackageVer = Expressions.GetText(Bytes, 0, VerLen).Substring(1, VerLen - 1);
+                        int OwnerLen = Expressions.GetByte(Bytes, 0) + 1;
+                        Pack.PackageOwner = Expressions.GetText(Bytes, 0, OwnerLen).Substring(1, OwnerLen - 1);
 
-                    //No NULs after file name, so let while loop read next FILE is exist or Stream not ended.
-                    //Before ending while, let's add file to collection...
-                    Pack.Files.Add(File);
+                        //Unfortunately, I don't know what that next byte are here for. So, I'll discard it.
+                        Expressions.GetBytes(Bytes, 0, 1);
+
+                        //We're done with DATA, now let's read next 3 null characters.
+                        Expressions.GetBytes(Bytes, 0, 3);
+
+                        //Now, we should have FILE. Ending this if else and continuing with a while loop.
+                    }
+                    else if(Header == "ELIF")
+                    { 
+                        TTFileClass File = new TTFileClass();
+                        File.FullNameLength = Expressions.GetByte(Bytes, 0) - 1;
+                        Expressions.GetBytes(Bytes, 0, 3); // 3 NULs...
+                        File.CRC = Expressions.GetCRCID(Expressions.GetBytes(Bytes, 0, 4));
+
+                        //Unfortunately, I don't know what these next 2 bytes are here for. So, I'll discarding them.
+                        //Note: A friend says it's file size. But multiplation, addition and XOR did not worked to get original file sizes.
+                        Expressions.GetBytes(Bytes, 0, 2);
+
+                        //Skipping 2 NULs...
+                        Expressions.GetBytes(Bytes, 0, 2);
+
+                        File.OriginalNameLength = Expressions.GetByte(Bytes, 0);
+                        File.FileName = Expressions.GetText(Bytes, 0, File.OriginalNameLength + 1).Substring(1, File.OriginalNameLength);
+
+                        //No NULs after file name, so let while loop read next FILE is exist or Stream not ended.
+                        //Before ending while, let's add file to collection...
+                        Pack.Files.Add(File);
+                    }
+                    else
+                    {
+                        //Hmm...
+                    }
                 }
 
                 //Return when everything is good to go.
@@ -175,7 +171,7 @@ namespace Package
         //Converts 4-byte CRC data into string and returns.
         public static string GetCRCID(byte[] Bytes)
         {
-            if (Bytes.Length != 4)
+            if(Bytes.Length != 4)
                 throw new ArgumentException("Invalid Package ID or formatting problem.");
 
             string Return = "";
